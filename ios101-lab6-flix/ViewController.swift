5deabbc4e239093ca9af382fd8fe3df6f5480c9d
michelleduong35 <michelleduong35@gmail.com>
2023-03-28 11:57:20 -0700

    Make release date and vote average properties optional

--- ios101-lab6-flix/ViewController.swift
+++ ios101-lab6-flix/ViewController.swift
@@ -64,8 +64,38 @@ class ViewController: UIViewController, UITableViewDataSource {
 
         // Assign table view data source
         tableView.dataSource = self
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
 
         fetchMovies()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // MARK: - Pass the selected movie data
+
+        // Get the index path for the selected row.
+        // `indexPathForSelectedRow` returns an optional `indexPath`, so we'll unwrap it with a guard.
+        guard let selectedIndexPath = tableView.indexPathForSelectedRow else { return }
+
+        // Get the selected movie from the movies array using the selected index path's row
+        let selectedMovie = movies[selectedIndexPath.row]
+
+        // Get access to the detail view controller via the segue's destination. (guard to unwrap the optional)
+        guard let detailViewController = segue.destination as? DetailViewController else { return }
+
+        detailViewController.movie = selectedMovie
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Customary to call the overridden method on `super` any time you override a method.
+        super.viewWillAppear(animated)
+
+        // get the index path for the selected row
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+
+            // Deselect the currently selected row
+            tableView.deselectRow(at: selectedIndexPath, animated: animated)
+        }
     }
 
     // Fetches a list of popular movies from the TMDB API
